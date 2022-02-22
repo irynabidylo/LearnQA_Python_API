1ed7834615dcd174c3b58b16424a72479c29d3c8
irynabidylo <74086729+irynabidylo@users.noreply.github.com>
2022-02-21 20:48:07 -0800

    Update lesson2_ex5.py

--- lesson2_ex5.py
+++ lesson2_ex5.py
@@ -1,5 +1,9 @@
-json_text = {"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},
-                         {"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}
+import json
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},' \
+            '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 
-json_value = json_text["messages"][1]
+
+json_parsed = json.loads(json_text)
+
+json_value = json_parsed["messages"][1]
 print(json_value["message"])
